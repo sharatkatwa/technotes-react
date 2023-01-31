642c8d894fb3faf07d40920b3e025d74a1a70a04
sharatkatwa <sjkatwa2000@gmail.com>
2023-01-31 16:36:28 +0530

    fixed a package import error while building the react app

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { disableReactDevtools } from '@fvilers/disable-react-devtools'
-
+import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
 
-if (process.env.NODE_ENV === 'production') disableReactDevtools()
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
